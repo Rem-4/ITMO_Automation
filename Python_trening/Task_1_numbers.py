913b8b777c7cfebec02f81702fe868cd8f4ef473
Rem <remalekseev12@gmail.ru>
2024-11-02 14:51:22 +0300

    First Commit

---
+++ Python_trening/Task_1_numbers.py
@@ -0,0 +1,11 @@
+a = 2
+print(a, "относиться к типу", type(a))
+
+
+b = 15.0001
+print(b, "относиться к типу", type(b))
+
+
+
+c = 1+2j
+print(c, "Комплексное число?", isinstance(c, complex))
---
+++ home_work/2_hw.py
@@ -0,0 +1,24 @@
+def task_1(a = 10, b = 4.2, c = "Hello", d = [1, 2, 3, 4], e = True) -> str:
+
+    print(type(a))
+    print(type(b))
+    print(type(c))
+    print(type(d))
+    print(type(e))
+    return ""
+
+print(task_1())
+
+
+
+def task_2(a = [1, 2, 3, 5, 8, 13, 21]) ->int:
+
+    return a[:3]
+
+print(task_2())
+
+def task_3(a = 4) ->int:
+
+    return a**2
+
+print(task_3())
